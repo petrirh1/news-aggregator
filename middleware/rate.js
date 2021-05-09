674003a6450f33e19a0211e57a661578addcc2a2
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-05-09 11:39:02 +0300

    Add rate limiter

---
+++ middleware/rate.js
@@ -0,0 +1,8 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 100 // limit each IP to 100 requests per windowMs
+});
+
+module.exports = limiter;
