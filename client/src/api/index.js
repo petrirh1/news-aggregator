97bd3766ea807263094ca1edd31b2ba314c0f398
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-05-16 15:52:08 +0300

    add caching

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
+import { shouldUseCache, storeInCache } from '../helpers/cache';
+import { hasStorage } from '../helpers/hasStorage';
 let cancelToken;
 
 export const fetchData = async (url, page, limit) => {
+	const cachedData = hasStorage() && sessionStorage.getItem(url);
+	url = url.replace('(', '').replace(')', '');
+
 	/*
 	 * Cancel token is used to cancel ongoing fetch request,
 	 * this is useful when category is changed before data has finished fetching
@@ -11,12 +16,19 @@ export const fetchData = async (url, page, limit) => {
 	}
 
 	cancelToken = axios.CancelToken.source();
-	url = url.replace('(', '').replace(')', '');
 
 	try {
+		if (hasStorage() && cachedData && shouldUseCache(cachedData, page)) {
+			return JSON.parse(cachedData);
+		}
+
 		const { data } = await axios.get(`/api/uutiset${url}?page=${page}&limit=${limit}`, {
-			cancelToken: cancelToken.token
+			cancelToken: cancelToken.token,
 		});
+
+		if (hasStorage()) {
+			storeInCache(url, data);
+		}
 
 		return data;
 	} catch (error) {
