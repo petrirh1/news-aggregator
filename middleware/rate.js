1688bc1fb1253a47facc9b8b8ed33f96196d367e
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-05-16 11:50:27 +0300

    remove comment

--- middleware/rate.js
+++ middleware/rate.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
-	max: 1000, // limit each IP to 100 requests per windowMs
+	max: 1000,
 });
 
 module.exports = limiter;
