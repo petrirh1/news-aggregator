33707252803fdb20a7acf4935c96155e15c138a8
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-06-03 19:10:57 +0300

    add tests

---
+++ client/src/tests/api.test.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+axios.defaults.adapter = require('axios/lib/adapters/http');
+const endpoint = 'http://localhost:5000/api/uutiset/';
+
+describe('api tests', () => {
+	test('Should not return duplicates', async () => {
+		sessionStorage.clear();
+
+		const batch1 = await axios.get(endpoint + 'uusimmat?page=1&limit=50');
+		const batch2 = await axios.get(endpoint + 'uusimmat?page=2&limit=50');
+		const batch3 = await axios.get(endpoint + 'uusimmat?page=3&limit=50');
+		const batch4 = await axios.get(endpoint + 'uusimmat?page=4&limit=50');
+		const batch5 = await axios.get(endpoint + 'uusimmat?page=5&limit=50');
+		const news = [
+			...batch1.data.docs,
+			...batch2.data.docs,
+			...batch3.data.docs,
+			...batch4.data.docs,
+			...batch5.data.docs,
+		];
+
+		const uniqueValues = new Set(news.map((el) => el.title));
+		const hasUniqueValuesOnly = uniqueValues.size < news.length;
+		sessionStorage.clear();
+
+		expect(hasUniqueValuesOnly).toBeFalsy();
+	});
+});
