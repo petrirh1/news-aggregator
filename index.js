245daa4dd98b8566d05d8d2d23f6f1819d1b5db8
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-07-20 20:01:57 +0300

    redirect

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const devRoute = require('./routes/devRoute');
 const dev = process.env.NODE_ENV === 'production' ? false : true;
 require('dotenv').config();
 
+app.get('*', (req, res, next) => {
+	if (req.headers.host.slice(0, 3) != 'www') {
+		res.redirect('https://www.' + req.headers.host + req.url, 301);
+	} else {
+		next();
+	}
+});
+
 app.use(
 	helmet({
 		contentSecurityPolicy: {
